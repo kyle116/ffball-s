d23a286c342fc8a6875cec9dd105c2f29a5d9c3a
Kyle Liu <kyle116@yahoo.com>
2017-12-21 10:40:11 -0800

    lobby routes

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ const
   cors = require('cors'),
   PORT = process.env.PORT || 3001,
   token = process.env.TOKEN,
-  mongoUrl = (process.env.MONGO_URL || 'mongodb://localhost/ffball-s')
-
+  mongoUrl = (process.env.MONGO_URL || 'mongodb://localhost/ffball-s'),
+  Lobby = require('./Lobby'),
+  List = require('./List'),
+  Team = require('./Team'),
+  Player = require('./Player')
+  
 // MongoDB Connection
 mongoose.connect(mongoUrl, (err) => {
   console.log(err || 'connected to MongoDB');
@@ -21,7 +25,31 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
+// Routes
+app.get('/', (req, res) => {
+  res.json({message: 'Root Route'});
+})
+
+app.route('/lobby')
+  .get((req, res) =>{
+    Lobby.find({}, (err, lobbies) =>{
+      res.json(lobbies);
+    })
+  })
+  .post((req, res) => {
+    Lobby.create(req.body, (err, lobby) =>{
+      res.json({success: true, message: "Lobby Created", lobby});
+    })
+  })
+
+app.route('/lobby/:id')
+  .get((req, res) => {
+    Lobby.findById(req.params.id, (err, lobby) => {
+      res.json(lobby);
+    })
+  })
+
 // Port
 app.listen(PORT, () => {
-  console.log(`server is litening on port ${PORT}`)
+  console.log(`server is litening on port ${PORT}`);
 })
